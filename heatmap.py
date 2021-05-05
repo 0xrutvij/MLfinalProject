41960f697d0dcca47a440499f6f06e1abd215d05
0xrutvij <54966871+0xrutvij@users.noreply.github.com>
2021-05-04 19:06:57 -0500

    corr regen and discrete hist

--- heatmap.py
+++ heatmap.py
@@ -16,22 +16,31 @@ def correlation_heatmap(train):
         plt.show()
 
 if __name__ == '__main__':
-    
-    feature_names = np.array(['contact_class_score_diff','contact_id','counter','delay','msg_delay','msg_delay_stamps'])
-    df = pd.read_csv('./processedData/aggregatedAndProcessed.csv')
-    x = df[['contact_class_score_diff','contact_id','counter','delay','msg_delay','msg_delay_stamps']]
-    y = df['contact_type']
-    df.drop('Unnamed: 0', axis=1, inplace=True)
-    correlation_heatmap(df)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=6375)
-    xgb = XGBRegressor(n_estimators=100)
-    xgb.fit(x_train, y_train)
-    x_test = np.ascontiguousarray(x_test)
-    y_test = np.ascontiguousarray(y_test)
-    print(xgb.feature_importances_)
+        
+        df = pd.read_csv('./processedData/aggregatedAndProcessed.csv')
+        df.rename(columns={
+        "contact_type":"contactType",
+        "contact_class_score_diff": "classScore", 
+        "contact_id": "deviceContacted", 
+        "msg_delay_stamps":"serverDelay", 
+        "msg_delay":"deviceDelay"}, inplace=True)
+        print(df.columns)
 
-    perm_importance = permutation_importance(xgb, x_test, y_test)
-    sorted_idx = perm_importance.importances_mean.argsort()
-    plt.barh(feature_names[sorted_idx], perm_importance.importances_mean[sorted_idx])
-    plt.show()
-    correlation_heatmap(x_train[feature_names[sorted_idx]])
+        feature_names = np.array(['classScore', 'deviceContacted', 'counter',
+       'delay', 'deviceDelay', 'serverDelay'])
+        x = df[['classScore', 'deviceContacted', 'counter','delay', 'deviceDelay', 'serverDelay']]
+        y = df['contactType']
+        df.drop('Unnamed: 0', axis=1, inplace=True)
+        correlation_heatmap(df)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=6375)
+        xgb = XGBRegressor(n_estimators=100)
+        xgb.fit(x_train, y_train)
+        x_test = np.ascontiguousarray(x_test)
+        y_test = np.ascontiguousarray(y_test)
+        print(xgb.feature_importances_)
+
+        perm_importance = permutation_importance(xgb, x_test, y_test)
+        sorted_idx = perm_importance.importances_mean.argsort()
+        plt.barh(feature_names[sorted_idx], perm_importance.importances_mean[sorted_idx])
+        plt.show()
+        correlation_heatmap(x_train[feature_names[sorted_idx]])
